1077f52cedba10a580ac435347d8d2518941460c
Leeroy Life <leeroy.leffk@web.de>
2026-03-16 18:34:29 +0100

    github action v pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // <-- Das schaltet das doppelte Laden ab
+  output: 'export',
 };
 
 export default nextConfig;
